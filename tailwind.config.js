c7b3bf58d89fe867be8561ef37925054a0d9000d
Vo Minh Trung <tovm24xinchao@gmail.com>
2023-12-03 11:20:56 +0700

    Add header.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./public/**/*.{html,js}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: ["./public/**/*.{html,js}"],
+    theme: {
+        extend: {
+            colors: {
+                'light-coffee': '#c89f94'
+            }
+        },
+    },
+    plugins: [],
 }
 
