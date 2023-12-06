35bcd14398bfd8633ebd1fd0cfde6f2c9d11386e
Vo Minh Trung <tovm24xinchao@gmail.com>
2023-12-06 21:58:52 +0700

    Add story and add subheadline.

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,9 +7,16 @@ module.exports = {
                 Karla: ['Karla','sans-serif']
             },
 
+
             colors: {
-                'light-coffee': '#c89f94'
+                'coffee': {
+                    50: '#e8d6d0',
+                    200: '#c89f94',
+                    400: '#a25f4b',
+                    600: '#744838'
+                }
             },
+
 
             keyframes: {
                 slideDown: {
@@ -17,6 +24,7 @@ module.exports = {
                     '100%': { transform: 'translateY(0)' },
                 }
             },
+
 
             animation: {
                 slideDown: 'slideDown .4s ease-in-out',
